b1499bba8a193bde97bc1380e43e75179ae6c009
danyonsatterlee <danyon.satterlee@gmail.com>
2017-08-12 16:32:01 -0600

    all styling done on sign up and on sketch game and match

--- src/components/sketch-game.js
+++ src/components/sketch-game.js
@@ -1,12 +1,26 @@
 import React from "react";
 
-
 class Sketch extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Sketch connected</h1>
+
+            <div className="col-md-12">
+                <h1 className="sketch">Draw Battle</h1>
+                <div className="sketch-pad-container">
+                    <h3 className="prompt">PROMPT APPEARS HERE INSTEAD OF THIS</h3>
+                    <div className="row">
+                        <div className="col-md-12 sketchpad">
+
+                        </div>
+                          <div className="col-md-12 text-center" >
+                             <input className="guess-input input-lg" placeholder="what is it?"/>
+                        <span className="guess-button">Guess</span>
+                        </div>
+                       
+                    </div>
+                </div>
             </div>
+
         )
     }
 }
--- src/containers/home-container.js
+++ src/containers/home-container.js
@@ -3,20 +3,18 @@ import SketchContainer from "../containers/sketch-game-container";
 import MatchmakeContainer from "./matchmake-container";
 import NavbarSketch from "../components/navbar.js";
 
-
 class HomeContainer extends React.Component {
     render() {
         return (
             <div>
-                   <NavbarSketch></NavbarSketch>
-        
-            <div className="container-fluid">
-             
-                <h1>HomeContainer connected</h1>
-                <MatchmakeContainer/>
-                <SketchContainer/>
-            </div>
+                <NavbarSketch></NavbarSketch>
+
+                <div className="container-fluid">
+
+                    <MatchmakeContainer/>
+                    <SketchContainer/>
                 </div>
+            </div>
         )
     }
 }
