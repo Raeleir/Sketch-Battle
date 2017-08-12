c9607dc50809f2ff4620495571e1efd3b29af2f0
danyonsatterlee <danyon.satterlee@gmail.com>
2017-08-12 10:18:11 -0600

    some style done for login page

---
+++ src/components/how-to.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HowTo extends React.Component {
+    render() {
+        return (
+            <div className="col-md-6">
+                <div className="how-to-flex ">
+
+                    <h1>Sketch Battle</h1>
+                    <p>battle out the best of the guess vs the best of the pen</p>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HowTo;
