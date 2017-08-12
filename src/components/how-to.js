3056e193704c57f7bf9bd605edbaafedd415baef
danyonsatterlee <danyon.satterlee@gmail.com>
2017-08-12 15:12:43 -0600

    finished styling

--- src/components/how-to.js
+++ src/components/how-to.js
@@ -3,12 +3,18 @@ import React from 'react';
 class HowTo extends React.Component {
     render() {
         return (
-            <div className="col-md-6">
-                <div className="how-to-flex ">
+            <div>
+                <div className="col-md-4 col-sm-offset-1">
+                    <div className="how-to-flex ">
 
-                    <h1>Sketch Battle</h1>
-                    <p>battle out the best of the guess vs the best of the pen</p>
+                        <h1>Sketch Battle</h1>
+                        <p>battle out the best of the guess vs the best of the pen</p>
 
+                    </div>
+
+                </div>
+                <div className="col-md-2">
+                    <div className="line center-block"></div>
                 </div>
             </div>
         );
--- src/components/log-in.js
+++ src/components/log-in.js
@@ -4,12 +4,12 @@ class Login extends React.Component {
     render() {
         return (
 
-            <div className="col-md-6">
+            <div className="col-md-4">
                 <div className="how-to-flex">
                     <h1>Login</h1>
                     <div className="form">
-                        <input placeholder="username"/>
-                        <input placeholder="password"/>
+                        <input autoFocus="autoFocus" className="login-inputs" placeholder="username"/>
+                        <input className="login-inputs" placeholder="password"/>
                         <div className="row">
                             <div className="col-md-6 text-center">
                                <Link to="/signup "><p className="create-account-link pull-left">Create an Account</p></Link> 
