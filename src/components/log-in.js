4a8a17b89e81744536b9cbf05fa8182a08ff024d
danyonsatterlee <danyon.satterlee@gmail.com>
2017-08-14 11:45:30 -0600

    login working

--- src/components/log-in.js
+++ src/components/log-in.js
@@ -7,18 +7,45 @@ class Login extends React.Component {
             <div className="col-md-4">
                 <div className="how-to-flex">
                     <h1>Login</h1>
-                    <div className="form">
-                        <input autoFocus="autoFocus" className="login-inputs" placeholder="username"/>
-                        <input className="login-inputs" placeholder="password"/>
+                    <form
+                        onSubmit={(event) => {
+                        event.preventDefault();
+                        this
+                            .props
+                            .handleSubmit(this.props.input.username, this.props.input.password)
+                    }}className="form">
+
+                        <input
+                            value={this.props.input.username}
+                            onChange={(event) => {
+                            this
+                                .props
+                                .handleChange("username", event)
+                        }}
+                            type="text"
+                            autoFocus="autoFocus"
+                            className="login-inputs"
+                            placeholder="username"/>
+
+                        <input
+                            value={this.props.input.password}
+                            onChange={(event) => {
+                            this
+                                .props
+                                .handleChange("password", event)
+                        }}
+                            type="password" placeholder="password"className="login-inputs"/>
                         <div className="row">
                             <div className="col-md-6 text-center">
-                               <Link to="/signup "><p className="create-account-link pull-left">Create an Account</p></Link> 
+                                <Link to="/signup ">
+                                    <p className="create-account-link pull-left">Create an Account</p>
+                                </Link>
                             </div>
                             <div className="col-md-6">
-                                <p className="pull-right login-button">Login</p>
+                                <button type="submit" className="pull-right login-button">Login</button>
                             </div>
                         </div>
-                    </div>
+                    </form>
 
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 import "./index.css";
 import HomeContainer from "./containers/home-container";
 
 import LoginContainer from "./containers/log-in-container";
 import SignupContainer from "./containers/sign-up-container";
 
-
+let store = createStore(reducers, applyMiddleware(thunk));
 
 class App extends React.Component {
     render() {
@@ -16,8 +20,8 @@ class App extends React.Component {
                 <div>
                  
                         <Switch>
-                            <Route exact path="/" component={HomeContainer}/>
-                            <Route exact path="/login" component={LoginContainer}/>
+                            <Route exact path="/home" component={HomeContainer}/>
+                            <Route exact path="/" component={LoginContainer}/>
                             <Route exact path="/signup" component={SignupContainer}/>
                         </Switch>
                 </div>
@@ -26,4 +30,5 @@ class App extends React.Component {
     }
 }
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+ReactDOM.render(
+    <Provider store={store}><App/></Provider>, document.getElementById('root'));
